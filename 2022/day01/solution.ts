f9c73c7f9ac33feecde5f87c8eb65e249c661e6a
k12ish <k4krish@gmail.com>
2022-12-04 18:00:02 +0000

    typo

--- 2022/day01/solution.ts
+++ 2022/day01/solution.ts
@@ -7,16 +7,16 @@ let calories: number[] = file
   .map(
     // find sum of each paragraph
     (lines) => {
-      lines
+      return lines
         .split("\n")
         .map((line) => parseInt(line.trim()) || 0)
         .reduce((sum, next) => sum + next, 0);
-    }
+    },
   )
   .sort((a, b) => b - a);
 
 console.log("Part one: ", calories[0]);
 console.log(
   "Part two: ",
-  calories.slice(0, 3).reduce((a, b) => a + b, 0)
+  calories.slice(0, 3).reduce((a, b) => a + b, 0),
 );
